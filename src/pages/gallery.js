a257ce5a29e5e337e7d4c2589e6c0af422f3ffde
madhushree1211 <madhushree.ska@gmail.com>
2025-06-08 20:48:54 +0530

    change pics in gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -4,8 +4,9 @@ export default function Gallery() {
       <h1>Gallery</h1>
       <p>Explore stunning images of festivals, monuments, and traditional attire.</p>
       <div className="gallery">
-        <img src="https://i.pinimg.com/736x/b2/0b/f0/b20bf03fed8b5226a4759d64877282d2.jpg" alt="Festivals" />
-        <img src="https://i.pinimg.com/736x/ea/f3/63/eaf3630df274e7b01afef0942d892355.jpg" alt="Traditional Attire" />
+        <img src="https://i.pinimg.com/736x/16/23/d0/1623d0817a308cdb3a8468f77f8bb155.jpg" alt="Tradition" />
+        <img src="https://i.pinimg.com/736x/cf/f4/04/cff404c2621348a77dc785dd313c7380.jpg" alt="Tradition" />
+        <img src="https://i.pinimg.com/736x/e6/dc/37/e6dc37920295dc06a1c69b83a32778d0.jpg" alt="Tradition" />
       </div>
     </div>
   );
