c227dd216af1a855d05b9e3cc142dd01a961a34a
madhushree1211 <madhushree.ska@gmail.com>
2025-06-08 19:34:32 +0530

    Fixed missing styles/globals.css issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'; // Make sure this path is correct
+import '../styles/globals.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
