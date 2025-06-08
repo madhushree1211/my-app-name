522b1ba6b67296d237588c0713246ff36409c697
madhushree1211 <madhushree.ska@gmail.com>
2025-06-08 19:44:05 +0530

    Fixed global CSS import issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
-import '../styles/globals.css';
-
+import '../styles/globals.css'; // ✅ Only in _app.js
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
+
 
